65b4924f76948317bd1bb7f9bdedc19043609854
padpadpadpad <d.padfieldscfc@gmail.com>
2017-07-18 16:34:01 +0100

    Adding first function and updating Readme - moveRmdOutput()

---
+++ R/moveRmdOutput.R
@@ -0,0 +1,19 @@
+#' moveRmdOutput
+#'
+#' move Rmarkdown output to a separate location
+#'
+#' automatically moves all .html, .pdf or .docx files from one folder to another. Designed for Rmarkdown knitted documents that are regularly rendered in the same folder as the .Rmd document
+#'
+#' @param Rmd_path path to Rmarkdown documents
+#' @param Output_path path to where the output (knitted) documents are going to be stored
+#' @return moves all of the files with the following extensions from Rmd_path to the Output_path: .html, .docx, .pdf
+#' @examples
+#' \dontrun{moveRmdOutput('Rmd_files', 'Output_files')}
+#' @export
+
+# Script to automatically move all .html or .pdf files or .docx files from Rmd_files to Output_files
+moveRmdOutput <- function(Rmd_path, Output_path){
+  output_files <- list.files(Rmd_path, pattern = c('.html|.pdf|.docx'))
+  sapply(output_files, function(x){file.rename(file.path(Rmd_path, x), file.path(Output_path, x))})
+}
+
